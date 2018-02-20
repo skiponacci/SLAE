ed8728d2ce296e81a61b44d49ff1e9898d5e2831
skiponacci <skiponacci@users.noreply.github.com>
2018-02-19 22:46:12 -0500

    Create objdump-compile.sh

---
+++ objdump-compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '[+] Assembling with Nasm ... '
+nasm -f elf32 -o $1.o $1.nasm
+
+echo '[+] Linking ...'
+ld -z execstack -o $1 $1.o
+
+echo '[+] Dumping Shellcode ... '
+objdump -d $1.o |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+
+echo '[+] Done!'
